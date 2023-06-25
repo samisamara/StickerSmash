fe43c626990488d4211b3435051cdae52cd51ab9
Sami Samara <samisamara99@yahoo.com>
2023-06-25 02:12:18 -0500

    added Button.js, added buttons to home page

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Image } from 'react-native';
+import { StyleSheet, View, Image, Pressable } from 'react-native';
 
+import Button from './components/Button';
 import ImageViewer from './components/ImageViewer';
 
 const PlaceholderImage = require('./assets/background-image.png');
@@ -10,6 +11,10 @@ export default function App() {
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer placeholderImageSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo" />
+        <Button label="Use this photo" />
       </View>
       <StatusBar style="auto" />
     </View>
@@ -21,15 +26,18 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
   },
   imageContainer: {
     flex: 1,
     paddingTop: 58,
   },
-  image:{
+  image: {
     width: 320,
     height: 440,
     borderRadius: 18,
+  },
+  footerContainer: {
+    flex: 1/3,
+    alignItems: 'center',
   },
 });
